6eb997b2150b197cf2e71ae08b345f34fd19e1ad
mwong068 <mwong068@gmail.com>
2020-04-25 13:49:45 -0700

    added full routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html 
-  resources :helpful_users, only: [:create]
+  resources :helpful_users
   resources :in_need_users
   resources :categories
   resources :helpful_items
