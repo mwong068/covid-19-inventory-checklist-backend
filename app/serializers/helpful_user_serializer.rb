78bc39cde3fbdfc62f47a4865e7b6ae0359f6a09
mwong068 <mwong068@gmail.com>
2020-04-25 14:51:25 -0700

    added serializer functonality to show items as listed with user

--- app/serializers/helpful_user_serializer.rb
+++ app/serializers/helpful_user_serializer.rb
@@ -1,4 +1,4 @@
 class HelpfulUserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :name, :location, :email, :family_size, :has_children, :can_deliver
+  attributes :id, :username, :name, :location, :email, :family_size, :has_children, :can_deliver, :helpful_items
 
 end
